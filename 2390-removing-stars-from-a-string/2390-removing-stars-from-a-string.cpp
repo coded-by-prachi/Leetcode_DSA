891a81014db30d85cb3a389c4efd408f0f7fd321
Prachi Prajapati <prizprajapati2714@gmail.com>
2026-05-26 19:38:16 +0530

    Time: 24 ms (28.34%), Space: 28.3 MB (72.03%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string str = "";
+        for(char ch: s){
+            if(ch == '*'){
+                str.pop_back();
+            }
+            else{
+                str.push_back(ch);
+            }
+            
+        }
+        return str;
+        
+    }
+};
