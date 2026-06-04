8953f1b8e23c12830c16e0d5244e88ea071a6063
Prachi Prajapati <prizprajapati2714@gmail.com>
2026-06-04 22:42:25 +0530

    Time: 0 ms (100%), Space: 8.6 MB (70.91%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-         if(dividend == INT_MIN && divisor == -1)
+        if(dividend == INT_MIN && divisor == -1)
              return INT_MAX;
         //  if(divisor == 1 && dividend == INT_MIN ){
         //     return  INT_MIN;
@@ -29,5 +29,6 @@ public:
         }
        
         return count;
+        
     }
 };
